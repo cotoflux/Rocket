605eaba91cfb8b4b4afb5c6dcb9845058eb94dfe
cotoflux <guaupelu@hotmail.com>
2020-06-10 18:48:42 +0200

    Start fase2

--- src/fase1/UseOfClases.java
+++ src/fase1/UseOfClases.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 
-public class UsoClases {
+public class UseOfClases {
 
 	
 	public static void main(String[] args) {
---
+++ src/fase2/Propeller.java
@@ -0,0 +1,17 @@
+package fase2;
+
+public class Propeller {
+	
+	private int codePropeller;
+	
+	Propeller(int codePropeller) {
+		this.codePropeller=codePropeller;
+	}
+
+	@Override
+	public String toString() {
+		return "Propeller [" + codePropeller + "]";
+	}
+
+
+}
