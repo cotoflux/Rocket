e52054f0076c189b8d26e8af40ffa1655389d7eb
cotoflux <guaupelu@hotmail.com>
2020-06-11 11:36:58 +0200

    Modification class Propellers with the new conditions

--- src/fase3/Propeller.java
+++ src/fase3/Propeller.java
@@ -3,14 +3,43 @@ package fase3;
 public class Propeller {
 	
 	private int codePropeller;
+	private int maxPower;
+	private int currentPower;
 	
+	public int getCodePropeller() {
+		return codePropeller;
+	}
+
+	public void setCodePropeller(int codePropeller, int maxPower, int currentPower) {
+		this.codePropeller = codePropeller;
+		this.maxPower = maxPower;
+		this.currentPower = currentPower;
+		
+	}
+
+	public int getMaxPower() {
+		return maxPower;
+	}
+
+	public void setMaxPower(int maxPower) {
+		this.maxPower = maxPower;
+	}
+
+	public int getCurrentPower() {
+		return currentPower;
+	}
+
+	public void setCurrentPower(int currentPower) {
+		this.currentPower = currentPower;
+	}
+
 	Propeller(int codePropeller) {
 		this.codePropeller=codePropeller;
 	}
 
 	@Override
 	public String toString() {
-		return " " + codePropeller;
+		return " [" + codePropeller + ", maxPower " + maxPower +",currentPower" + currentPower;
 	}
 
 
